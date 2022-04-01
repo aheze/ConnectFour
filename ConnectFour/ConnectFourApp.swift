119c5af5e45fd4aadeafd3abbe9bf1b352915da7
Zheng <aheze@getfind.app>
2022-04-01 09:38:18 -0700

    Initial Commit

---
+++ ConnectFour/ConnectFourApp.swift
@@ -0,0 +1,19 @@
+//
+//  ConnectFourApp.swift
+//  ConnectFour
+//
+//  Created by A. Zheng (github.com/aheze) on 4/1/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct ConnectFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConnectFour/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ConnectFour
+//
+//  Created by A. Zheng (github.com/aheze) on 4/1/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
